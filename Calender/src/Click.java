1913c1d78791348f9aef9fbdecfec6c7510035ef
gustjd <gustjd015@naver.com>
2015-11-29 23:09:37 +0900

    Calender(hy)

--- Calender/src/Click.java
+++ Calender/src/Click.java
@@ -11,7 +11,9 @@ class Click extends MouseAdapter {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
+		// 마우스 클릭시 행의 인덱스를 얻어옴
 		int row = tb2.tb.rowAtPoint(e.getPoint());
+		// 마우스 클릭시 컬럼의 인덱스를 얻어옴
 		int col = tb2.tb.columnAtPoint(e.getPoint());
 
 		// 선택한 셀의 저장된 값
@@ -19,16 +21,15 @@ class Click extends MouseAdapter {
 		// 마지막 줄의 row에서 10이하의 수가 있을 경우 다음달 값을 가져옴
 		if (row >= 0 && col >= 0) {
 			value = tb2.tb.getValueAt(row, col); // 클릭한 cell의 클래스를 얻어옴
-			System.out.println(value);
-			
+			System.out.println("value 값 : " + value);
 			
 			// System.out.println(tm1.getDate(row, col));
 			// 다이얼로그를 열어서 텍스트정보를 입력하고
 			// 선택한 월의 폴더를 생성
 			// 선택한 일의 text를 생성
 			int day = (int) value;
-			System.out.println("월 : " + CModel.calendar.get(Calendar.MONTH));
-			System.out.println("row 값은 : " + row + "day 값은 : " + day);
+			// System.out.println("월 : " + CModel.calendar.get(Calendar.MONTH));
+			// System.out.println("row 값은 : " + row + "day 값은 : " + day);
 			if(day >= 20 && row == 0){
 				CModel.getThismonth = CModel.calendar.get(Calendar.MONTH) + 1;
 			}
